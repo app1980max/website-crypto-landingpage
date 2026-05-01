1fc44f0b3b91fe8cda28ac718d2dc962d205fec3
app1980max <23049337+app1980max@users.noreply.github.com>
2026-05-01 23:35:04 +0300

    update

--- nextjs/src/components/Layout/Header/Navigation/menuData.tsx
+++ nextjs/src/components/Layout/Header/Navigation/menuData.tsx
@@ -6,5 +6,4 @@ export const headerData: HeaderItem[] = [
   { label: "Services", href: "/#portfolio" },
   { label: "Why Crypgo", href: "/#upgrade" },
   { label: "FAQs", href: "/#faq" },
-  { label: "Test", href: "/#test" },
 ];
